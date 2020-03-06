f73a21cc6c38b74e887ea1ce869fada708a1ee57
Kevin Van Lierde <kevin.van.lierde@conict.be>
2020-03-06 16:05:02 +0100

    Remove unnecessary templates, add landingpage data

--- src/includes/data/index.js
+++ src/includes/data/index.js
@@ -6,7 +6,27 @@ const hostedBy_en = require('./hosted-by_en.json');
 const hostedBy_nl = require('./hosted-by_nl.json');
 const intranet_nl = require('./intranet_nl.json');
 const intranet_en = require('./intranet_en.json');
+const landingpage_nl = require('./landingpage_nl.json');
+const landingpage_en = require('./landingpage_en.json');
 
 module.exports = {
-  data: [kulak_en, kulak_nl, generic_nl, generic_en, hostedBy_nl, hostedBy_en, intranet_nl, intranet_en]
+  data: [
+    kulak_nl,
+    kulak_en,
+    generic_nl,
+    generic_en,
+    hostedBy_nl,
+    hostedBy_en,
+    intranet_nl,
+    intranet_en,
+    landingpage_nl,
+    landingpage_en
+  ],
+  langs: {
+    general: { nl: generic_nl, en: generic_en },
+    kulak: { nl: kulak_nl, en: kulak_en },
+    'hosted-by': { nl: hostedBy_nl, en: hostedBy_en },
+    intranet: { nl: intranet_nl, en: intranet_en },
+    landingpage: { nl: landingpage_nl, en: landingpage_en }
+  }
 };
