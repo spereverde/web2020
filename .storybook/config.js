3066eee83baccb6e0e1475a49a5d4160acb0459f
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2021-06-01 09:54:46 +0200

    Update dependencies
    Signed-off-by: Kevin Van Lierde <kevin.van.lierde@gmail.com>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,3 @@
 import { configure, addDecorator, addParameters } from '@storybook/html';
 
-// the line below is required due to a bug in storybook-readme
-addParameters({ options: {} });
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
