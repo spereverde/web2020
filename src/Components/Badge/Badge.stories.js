867cda601541fc9f6dcbf2dbd3aad34c2fa2a531
Kim Paulissen <kim.paulissen@kuleuven.be>
2021-11-22 15:33:13 +0100

    campuslabels capitilize

--- src/Components/Badge/Badge.stories.js
+++ src/Components/Badge/Badge.stories.js
@@ -24,22 +24,27 @@ const themeVariants = [
 ];
 
 function Badge(args) {
-  return render(template({ ...defaultArgs, ...({
-    ...args,
-    modifiers: [
-      args.color ? `text-${args.color}` : '',
-      args.bgcolor ? `bg-${args.bgcolor}` : ''
-    ].join(' ').replace(/\s+/g, ' ')
-  }) }))
+  return render(
+    template({
+      ...defaultArgs,
+      ...{
+        ...args,
+        modifiers: [args.color ? `text-${args.color}` : '', args.bgcolor ? `bg-${args.bgcolor}` : '']
+          .join(' ')
+          .replace(/\s+/g, ' ')
+      }
+    })
+  );
 }
 
-export const Variants = (args) => render(
+export const Variants = (args) =>
+  render(
     Badge({
       contents: args.contents,
       bgcolor: args.bgcolor,
       color: args.color
     }).innerHTML
-);
+  );
 
 Variants.args = {
   contents: 'Unread',
@@ -47,44 +52,49 @@ Variants.args = {
   color: ''
 };
 
-
 /**
  * A badge can have any background colors
  */
-export const BackgroundColors = () => render(
-  themeVariants.map(variant => 
-    Badge({
-      contents: 'Unread',
-      bgcolor: variant,
-      color: variant.match(/^link|light$/) ? 'dark' : ''
-    }).innerHTML
-  ).join('\n')
-);
+export const BackgroundColors = () =>
+  render(
+    themeVariants
+      .map(
+        (variant) =>
+          Badge({
+            contents: 'Unread',
+            bgcolor: variant,
+            color: variant.match(/^link|light$/) ? 'dark' : ''
+          }).innerHTML
+      )
+      .join('\n')
+  );
 
 /**
  * A badge can have an icon
  */
-export const BadgeWithIcon = () => render(
-  [
-    Badge({
-      contents: 'Unlock <i class="material-icons">lock</i>',
-      bgcolor: 'tertiary'
-    }).innerHTML,
-    Badge({
-      contents: '<i class="material-icons">error_outline</i> important',
-      bgcolor: 'primary'
-    }).innerHTML
-  ].join('\n')
-);
+export const BadgeWithIcon = () =>
+  render(
+    [
+      Badge({
+        contents: 'Unlock <i class="material-icons">lock</i>',
+        bgcolor: 'tertiary'
+      }).innerHTML,
+      Badge({
+        contents: '<i class="material-icons">error_outline</i> important',
+        bgcolor: 'primary'
+      }).innerHTML
+    ].join('\n')
+  );
 
-export const CampusBadge = (args) => render(
-`<ul class="list-unstyled campus-labels text-uppercase">
+export const CampusBadge = (args) =>
+  render(
+    `<ul class="list-unstyled campus-labels text-capitilize">
   <li><span class="badge bg-tertiary"><i class="material-icons">place</i> Alle campussen buiten Leuven</span></li>
   <li><span class="badge bg-tertiary"><i class="material-icons">place</i> Campussen Leuven</span></li>
   <li><span class="badge bg-tertiary"><i class="material-icons">place</i> Campus Brussel</span></li>
   <li><span class="badge bg-tertiary"><i class="material-icons">place</i> Campus Sint-Lucas Gent</span></li>
 </ul>`
-);
+  );
 
 export default {
   title: 'Components/Badge',
@@ -92,20 +102,18 @@ export default {
   argTypes: {
     contents: {
       control: 'text',
-      table: { defaultValue: { summary: defaultArgs.contents }}
+      table: { defaultValue: { summary: defaultArgs.contents } }
     },
     bgcolor: {
       control: 'select',
       options: themeVariants,
-      table: { defaultValue: { summary: defaultArgs.bgcolor }}
+      table: { defaultValue: { summary: defaultArgs.bgcolor } }
     },
     color: {
       control: 'select',
       options: themeVariants,
-      table: { defaultValue: { summary: defaultArgs.color }}
+      table: { defaultValue: { summary: defaultArgs.color } }
     }
   },
-  parameters: {
-
-  }
-}
+  parameters: {}
+};
